aa389f9846118aa6516b84b53e7f7a55c8a6f950
Alex McCabe <alexwmccabe@gmail.com>
2018-06-14 15:21:16 +0100

    Use config for port if available

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('module-alias/register');
+const config = require('config');
 
 const app = require('@/app');
 const server = require('@/server');
 require('@/routes')(app);
 
+app.set('port', process.env.PORT || config.get('app.port') || 5000);
 
 server
-    .listen(app)
+    .listen(app, app.get('port'))
     .then(port => console.log('Server listening on port %s', port));
