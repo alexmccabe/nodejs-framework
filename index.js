d6c32e5bcc3059a074cd43fac0fb4ba7697f09a4
Alex McCabe <alexwmccabe@gmail.com>
2018-06-15 17:03:33 +0100

    Add: more colourful logging

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('module-alias/register');
+const chalk = require('chalk');
 
 const app = require('@/app');
 const server = require('@/server');
 
 app.set('port', process.env.PORT || 5000);
 
-server
-    .listen(app, app.get('port'))
-    .then(port => console.log('Server listening on port %s', port));
+server.listen(app, app.get('port')).then(port => {
+    console.log('');
+    console.log(
+        chalk.bgBlue.black(' Server listening on port: '),
+        chalk.blue(port)
+    );
+});
