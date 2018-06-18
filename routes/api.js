7eb53837c06aa69cef35647b5204763d8e043b4c
Alex McCabe <alexwmccabe@gmail.com>
2018-06-18 14:25:31 +0100

    Add: api routes

---
+++ routes/api.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/current-user', (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.send(req.user);
+    }
+
+    return res.sendStatus(403);
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+
+    return res.redirect('/');
+});
+
+module.exports = router;
