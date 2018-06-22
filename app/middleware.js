b8ac0ce63dc7e39b255364832e7cf966c2547c22
Alex McCabe <alexwmccabe@gmail.com>
2018-06-22 11:24:11 +0100

    Refactor: routing
    - Removed the auto requirement of routes in favour of
      a move explicit route declaration.
    - Add sub-route for /api requests
    - Add app/middleware.js to handle all global middleware

---
+++ app/middleware.js
@@ -0,0 +1,41 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const config = require('config');
+
+const { isAllowedOrigin } = require('@/utilities');
+
+/**
+ * Set the required headers for the application
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ */
+function setHeaders(req, res, next) {
+    const origin = req.headers.origin;
+
+    if (isAllowedOrigin(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    // Request methods you wish to allow
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With,Content-Type'
+    );
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+}
+
+module.exports = app => {
+    app.use((res, req, next) => setHeaders(res, req, next));
+    app.use(express.static('public'));
+    app.use(bodyParser.json(config.get('bodyParser')));
+};
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,26 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const fs = require('fs');
+// const fs = require('fs');
+const setupMiddleware = require('@/app/middleware');
 
 module.exports = app => {
-    app.use((req, res, next) => {
-        // const origin = req.headers.origin;
-        // const allowedOrigins = config.get('allowedOrigins');
+    setupMiddleware(app);
 
-        // if (allowedOrigins.indexOf(origin) > -1) {
-        //     res.setHeader('Access-Control-Allow-Origin', origin);
-        // }
-        res.setHeader('Access-Control-Allow-Origin', '*');
+    app.use('/', require('./root'));
+    app.use('/api', require('./api'));
+    app.use('/auth', require('./auth'));
 
-        // Request methods you wish to allow
-        res.setHeader(
-            'Access-Control-Allow-Methods',
-            'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-        );
+    // This was used to automatically mount routes, now deprecated
+    // fs.readdirSync(__dirname).forEach(fileName => {
+    //     const filePath = `${__dirname}/${fileName}`;
+    //     const route = fileName.split('.')[0];
 
-        // Request headers you wish to allow
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'X-Requested-With,content-type'
-        );
+    //     if (fileName === 'index.js' || fs.lstatSync(filePath).isDirectory()) {
+    //         return;
+    //     }
 
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-
-        next();
-    });
-
-    app.use(express.static('public'));
-    app.use(
-        bodyParser.json({
-            limit: '50mb'
-        })
-    );
-
-    fs.readdirSync(__dirname).forEach(fileName => {
-        const filePath = `${__dirname}/${fileName}`;
-        const route = fileName.split('.')[0];
-
-        if (fileName === 'index.js' || fs.lstatSync(filePath).isDirectory()) {
-            return;
-        }
-
-        if (route !== 'root') {
-            app.use(`/${route}`, require(filePath));
-        } else {
-            app.use('/', require(filePath));
-        }
-    });
+    //     if (route !== 'root') {
+    //         app.use(`/${route}`, require(filePath));
+    //     } else {
+    //         app.use('/', require(filePath));
+    //     }
+    // });
 };
