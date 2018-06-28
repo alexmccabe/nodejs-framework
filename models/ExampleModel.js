32b9ea636998d9f75fc6c0143ac1912528a4d04e
Alex McCabe <alexwmccabe@gmail.com>
2018-06-28 10:13:57 +0100

    Add: virtual "id" field for easier comparison

--- models/ExampleModel.js
+++ models/ExampleModel.js
@@ -8,4 +8,8 @@ const schema = new Schema({
     vehicle: { type: Schema.Types.ObjectId, ref: 'vehicles' }
 });
 
+schema.virtual('id').get(function() {
+    return this._id.toString();
+});
+
 module.exports = mongoose.model('examples', schema);
