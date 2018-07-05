6bf1fa7b164d93e0e1a152a8dce6a7b588c34111
Alex McCabe <alexwmccabe@gmail.com>
2018-07-05 10:25:17 +0100

    Refactor: Middleware into smaller files with comments

---
+++ modules/middleware/setupGlobalMiddleware.js
@@ -0,0 +1,38 @@
+const bodyParser = require('body-parser');
+const config = require('config');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const store = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    collection: 'sessions'
+});
+
+const { setSecurityHeaders, setHeaders } = require('./setHeaders');
+
+module.exports = app => {
+    app.use(cookieParser());
+    app.use(
+        session({
+            secret: process.env.COOKIE_SECRET,
+            saveUninitialized: false,
+            resave: false,
+            cookie: { secure: false },
+            store
+        })
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    setSecurityHeaders(app);
+
+    app.use((res, req, next) => setHeaders(res, req, next));
+    app.use(express.static('public'));
+
+    app.use(bodyParser.urlencoded(config.get('bodyParser.urlencoded')));
+    app.use(bodyParser.json(config.get('bodyParser.json')));
+};
