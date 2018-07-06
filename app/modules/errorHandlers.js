4948666b765587e93f323303d0315f985d7adbf8
Alex McCabe <alexwmccabe@gmail.com>
2018-07-06 15:43:45 +0100

    Add: youch error reporting HTML on dev

--- app/modules/errorHandlers.js
+++ app/modules/errorHandlers.js
@@ -1,5 +1,6 @@
 const PrettyError = require('pretty-error');
 const pe = new PrettyError();
+const Youch = require('youch');
 
 module.exports = {
     notFound(req, res) {
@@ -33,8 +34,18 @@ module.exports = {
         if (error) {
             console.error(pe.render(error));
 
-            res.statusCode === 200 ? res.status(500) : null;
-            return res.send(error.message || error.toString());
+            if (process.env.NODE_ENV !== 'production') {
+                const youch = new Youch(error, req);
+
+                return youch.toHTML().then(html => {
+                    res.writeHead(200, { 'content-type': 'text/html' });
+                    res.write(html);
+                    res.end();
+                });
+            } else {
+                res.statusCode === 200 ? res.status(500) : null;
+                return res.send(error.message || error.toString());
+            }
         }
 
         return next();
