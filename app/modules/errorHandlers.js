364af4f042e22945654280d5bd2025c9a4f5f270
Alex McCabe <alexwmccabe@gmail.com>
2018-07-06 15:11:26 +0100

    Add: Better error handling
    - Error handling for API and regular routes
    - Don't send full errors stack to users
    - Add 404 route error handler

--- app/modules/errorHandlers.js
+++ app/modules/errorHandlers.js
@@ -1,9 +1,43 @@
+const PrettyError = require('pretty-error');
+const pe = new PrettyError();
+
 module.exports = {
+    notFound(req, res) {
+        res.status(404);
+
+        if (req.accepts('html')) {
+            return res.render('errors/404', { url: req.url });
+        }
+
+        // respond with json
+        if (req.accepts('json')) {
+            return res.send({ error: 'Not found' });
+        }
+
+        // default to plain-text. send()
+        return res.type('txt').send('Not found');
+    },
+
     apiErrorHandler(error, req, res, next) {
         if (error) {
-            console.error(error.stack);
+            console.error(pe.render(error));
 
-            res.status(500).send(error.message || error.toString());
+            res.statusCode === 200 ? res.status(500) : null;
+            return res.json(error.message || error.toString());
         }
+
+        return next();
+    },
+
+    errorHandler(error, req, res, next) {
+        console.log('no here');
+        if (error) {
+            console.error(pe.render(error));
+
+            res.statusCode === 200 ? res.status(500) : null;
+            return res.send(error.message || error.toString());
+        }
+
+        return next();
     }
 };
