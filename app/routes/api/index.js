2dd4b456ad21e1c982cc1ec0f287b481d5ed403e
Alex McCabe <alexwmccabe@gmail.com>
2018-07-06 15:16:14 +0100

    Add: 404 route handler

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const userRouter = require('./user');
-const { apiErrorHandler } = require('@/app/modules/errorHandlers');
+const { apiErrorHandler, notFound } = require('@/app/modules/errorHandlers');
 
 const router = express.Router();
 
-router.use('/user', userRouter);
-router.use('/example', require('@/app/routes/example'));
+router.use('/user', require('./user'));
+router.use('/example', require('./example'));
+router.use(notFound);
 router.use(apiErrorHandler);
 
 module.exports = router;
