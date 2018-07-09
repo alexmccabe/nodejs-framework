151b6ed269ccb05abc777e7d97258a0e7735f2ea
Alex McCabe <alexwmccabe@gmail.com>
2018-07-09 16:12:16 +0100

    Add: Route for csrf-token-meta-demo

--- app/routes/default.js
+++ app/routes/default.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const cheerio = require('cheerio');
+
+router.get('/csrf-token-meta-demo', (req, res, next) => {
+    fs.readFile('public/csrf-token-meta-demo.html', (err, html) => {
+        if (err) {
+            return next(err);
+        }
+
+        const $ = cheerio.load(html);
+
+        $('head').append(
+            `<meta name="csrf-token" content="${req.csrfToken()}">`
+        );
+
+        return res.send($.html());
+    });
+});
 
 router.get('/', (req, res) => {
     res.send('Home');
