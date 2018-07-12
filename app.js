fe074edbf346251b8e9360e4c17f2505d817ef5f
Alex McCabe <alexwmccabe@gmail.com>
2018-07-12 13:05:55 +0100

    Update: Comment out cluster code for use with PM2.

---
+++ app.js
@@ -0,0 +1,36 @@
+// const cluster = require('cluster');
+const config = require('config');
+const path = require('path');
+const fs = require('fs');
+
+require('module-alias/register');
+
+// if (cluster.isMaster) {
+//     const { getCpuCount } = require('@/app/utilities');
+//     const scriptPath = path.join(__dirname, config.app.paths.masterScript);
+
+//     fs.access(scriptPath, fs.constants.F_OK, err => {
+//         if (!err) {
+//             require(scriptPath)();
+//         }
+//     });
+
+//     for (let i = 0; i < getCpuCount(); i += 1) {
+//         cluster.fork();
+//     }
+// } else {
+const scriptPath = path.join(__dirname, config.app.paths.threadScript);
+
+fs.access(scriptPath, fs.constants.F_OK, err => {
+    if (!err) {
+        require(scriptPath)(process.env.INSTANCEID);
+    }
+});
+// }
+
+// cluster.on('exit', worker => {
+//     if (process.env.NODE_ENV === 'production') {
+//         console.log('Worker %d died, restarting...', worker.id);
+//         return cluster.fork();
+//     }
+// });
